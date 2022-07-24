08aef16051e436118aa0a4a9eb7e9e68be403a17
Vladyslav Bardin <vbardin810@gmail.com>
2022-07-25 00:39:44 +0300

    Add swagger attributes

--- src/Genesis.Case/Api/Controllers/RateController.cs
+++ src/Genesis.Case/Api/Controllers/RateController.cs
@@ -14,10 +14,27 @@ public class RateController : ControllerBase
         _exchangeRateService = exchangeRateService;
     }
 
+    /// <summary>
+    /// Get current BTC to UAH exchange rate
+    /// </summary>
+    /// <returns> Current BTC to UAH exchange rate </returns>
+    /// <response code="200"> Returns current BTC to UAH exchange rate </response>
+    /// <response code="400"> Any errors </response>
     [HttpGet]
     [Route("")]
+    [Produces("application/json")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<decimal> GetBtcExchangeRate()
     {
-        return await _exchangeRateService.GetCurrentBtcToUahExchangeRateAsync();
+        try
+        {
+            return await _exchangeRateService.GetCurrentBtcToUahExchangeRateAsync();
+        }
+        catch
+        {
+            HttpContext.Response.StatusCode = 400;
+            return decimal.MinusOne;
+        }
     }
 }
--- src/Genesis.Case/Api/Program.cs
+++ src/Genesis.Case/Api/Program.cs
@@ -1,4 +1,6 @@
+using System.Reflection;
 using Core;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,18 @@ builder.Services.AddCoreLogic();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "My API",
+        Description = "An ASP.NET Core Web API for receiving BTC to UAH exchange rate, and notify functionality."
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFile));
+});
 
 var app = builder.Build();
 
